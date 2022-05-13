957045c1c71a309cd9d6f7830924dd086d9459bc
Rigoberto Calderon <jesuscalflo15@gmail.com>
2022-05-13 00:28:20 -0500

    Creando list builder.

--- lib/screens/direcciones_pages.dart
+++ lib/screens/direcciones_pages.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/scan_provider.dart';
 
 class DireccionesPages extends StatelessWidget {
-   
   const DireccionesPages({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('DireccionesPages'),
-      ),
-    );
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: true);
+    scanListProvider.cargarScansTipo('http');
+    return ListView.builder(
+        itemCount: scanListProvider.scans.length,
+        itemBuilder: (_, index) => ListTile(
+              leading: Icon(
+                Icons.map,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text(scanListProvider.scans[index].valor),
+              subtitle: Text(scanListProvider.scans[index].id.toString()),
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+              ),
+              onTap: () => null,
+            ));
   }
 }
