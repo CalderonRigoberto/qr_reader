b027e3615fbe6d9db3213b4e7b39717ffce39e21
Rigoberto Calderon <jesuscalflo15@gmail.com>
2022-05-13 00:28:56 -0500

    Retronando list tile.

--- lib/screens/mapas_pages.dart
+++ lib/screens/mapas_pages.dart
@@ -1,12 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/scan_provider.dart';
 
 class MapasPages extends StatelessWidget {
   const MapasPages({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Mapas Pages'),
-    );
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: true);
+    scanListProvider.cargarScansTipo('geo');
+    return ListView.builder(
+        itemCount: scanListProvider.scans.length,
+        itemBuilder: (_, index) => ListTile(
+              leading: Icon(
+                Icons.map,
+                color: Theme.of(context).primaryColor,
+              ),
+              title: Text(scanListProvider.scans[index].valor),
+              subtitle: Text(scanListProvider.scans[index].id.toString()),
+              trailing: const Icon(
+                Icons.keyboard_arrow_right,
+                color: Colors.grey,
+              ),
+              onTap: () => null,
+            ));
   }
 }
