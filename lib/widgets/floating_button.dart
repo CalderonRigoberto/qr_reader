3e6a6eaac9f893b0a6511bc26153d70d796a7688
Rigoberto Calderon <jesuscalflo15@gmail.com>
2022-05-13 00:29:10 -0500

    Haciendo prueba de registros.

--- lib/widgets/floating_button.dart
+++ lib/widgets/floating_button.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_reader/providers/scan_provider.dart';
 
 class FloatingButton extends StatelessWidget {
   const FloatingButton({Key? key}) : super(key: key);
@@ -11,10 +13,14 @@ class FloatingButton extends StatelessWidget {
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
       onPressed: () async {
-        String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-            '#3D8BEF', 'Cancelar', false, ScanMode.QR);
+        /**String barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+            '#3D8BEF', 'Cancelar', false, ScanMode.QR); */
+
+        final scanListProvider =
+            Provider.of<ScanListProvider>(context, listen: false);
+        //scanListProvider.nuevoScan(barcodeScanRes);
+        scanListProvider.nuevoScan('https://google.com.mx');
       },
     );
   }
 }
- 
